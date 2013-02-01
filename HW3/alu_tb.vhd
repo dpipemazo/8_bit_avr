4a5206f323584f38f1d6397593ef85bbe541e502
dpipemazo <dpipemazo@gmail.com>
2013-01-31 18:01:45 -0800

    Fixed up the ALU a tad and wrote a fuckton of test vector code. Still need to write about half of the instructions for vector code

--- HW3/alu_tb.vhd
+++ HW3/alu_tb.vhd
@@ -144,6 +144,7 @@ begin
         variable rand_inptA, rand_inptB, expected: std_logic_vector(7 downto 0);
         variable op : curr_op;
         variable temp_op : std_logic_vector(15 downto 0);
+        variable check_bit : std_logic;
 
     begin
 
@@ -284,6 +285,9 @@ begin
                 --
                 elsif ( op = OP_AND ) then
 
+                    -- Put the correct instruction on the bus
+                    IR <= OpAND;
+
                     -- Do the AND
                     expected_result = rand_inptA and rand_inptB;
 
@@ -293,11 +297,310 @@ begin
                     assert(result = expected_result) report "Wrong answer random input OpAND test";
 
                 --
-                --
+                -- INSTRUCTION: ANDI
                 --
                 elsif ( op = OP_ANDI ) then
 
-                    -- Create the 
+                    -- Create the instruction word
+                    temp_op := OpANDI;
+
+                    temp_op(3 downto 0) := rand_inptB(3 downto 0);
+                    temp_op(11 downto 8) := rand_inptB(7 downto 4);
+
+                    -- Put the instruction word on the bus
+                    IR <= temp_op;
+
+                    -- Do the AND
+                    expected_result = rand_inptA and rand_inptB;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                    -- check the answer
+                    assert(result = expected_result) report "Wrong answer random input OpANDI test";
+
+                --
+                -- INSTRUCTION: ASR
+                --
+                elsif ( op = OP_ASR ) then
+
+                    -- Put the instruction word on the bus
+                    IR <= OpASR;
+
+                    -- Do the shift
+                    expected_result(6 downto 0) := rand_inptA(7 downto 1);
+                    expected_result(7) := rand_inptA(7);
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                    -- check the answer
+                    assert(result = expected_result) report "Wrong answer random input OpASR test";
+
+                --
+                -- INSTRUCTION: BCLR
+                --
+                elsif ( op = OP_BCLR ) then
+
+                    -- Need to build up the instruction word
+                    temp_op := OpBCLR;
+                    -- Use the bottom 3 bits of random input A to 
+                    --  choose which bit of the status register to 
+                    --  clear
+                    temp_op(6 downto 4) := rand_inptA(2 downto 0);
+
+                    -- Put the instruction on the instruction bus
+                    IR <= temp_op;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                    -- check the answer
+                    assert(StatReg(conv_integer(rand_inptA(2 downto 0))) = '0') report "Wrong answer random input OpBCLR test";
+
+
+                --
+                -- INSTRUCTION: BLD
+                --
+                elsif ( op = OP_BLD ) then
+
+                    -- Need to build up the instruction word
+                    temp_op := OpBLD;
+                    temp_op(2 downto 0) := rand_inptB(2 downto 0);
+
+                    -- Make a note of the T register
+                    check_bit := StatReg(6);
+
+                    -- Put the instruction word on the IR bus
+                    IR <= temp_op;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                    -- check the answer
+                    assert(result(conv_integer(rand_inptB(2 downto 0))) = check_bit) report "Wrond answer random input OpBLD test"
+
+                --
+                -- INSTRUCTION: BSET
+                --
+                elsif( op = OP_BSET ) then
+
+                    -- Need to build up the instruction word
+                    temp_op := OpBSET;
+                    -- Use the bottom 3 bits of random input A to 
+                    --  choose which bit of the status register to 
+                    --  clear
+                    temp_op(6 downto 4) := rand_inptA(2 downto 0);
+
+                    -- Put the instruction on the instruction bus
+                    IR <= temp_op;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                    -- check the answer
+                    assert(StatReg(conv_integer(rand_inptA(2 downto 0))) = '1') report "Wrong answer random input OpBCLR test";
+
+                --
+                -- INSTRUCTION: BST
+                --
+                elsif ( op = OP_BST ) then
+
+                    -- Need to build up the instruction word
+                    temp_op := OpBST;
+                    temp_op(2 downto 0) := rand_inptB(2 downto 0);
+
+                    -- Put the instruction on the instruction bus
+                    IR <= temp_op;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                    -- check the answer
+                    assert(StatReg(conv_integer(rand_inptB(2 downto 0))) = rand_inptA(conv_integer(rand_inptB(2 downto 0))) ) report "Wrong answer random input OpBST test";
+
+                --
+                -- INSTRUCTION: COM
+                --
+                elsif ( op = OP_COM ) then 
+
+                    -- Put the instruction word on the bus
+                    IR <= OpCom;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                    -- Check the answer
+                    assert(conv_integer(result) = (-1*int_randA + 1) + 1) report "Wrong answer random input OpCOM test";
+
+                --
+                -- INSTRUCTION: CP
+                --
+                elsif( op = OP_CP ) then
+                    -- Put the instruction word on the bus
+                    IR <= OpCP
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                --
+                -- INSTRUCTION: CPC
+                --
+                elsif ( op = OP_CPC )
+
+                    -- Put the instruction word on the bus
+                    IR <= OpCPC;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                --
+                -- INSTRUCTION: CPI
+                --
+                elsif ( op = OP_CPI ) 
+
+                    -- Build up the instruction word
+                    temp_op := OpCPI;
+                    temp_op(3 downto 0) := rand_inptB(3 downto 0);
+                    temp_op(11 downto 8) := rand_inptB(7 downto 4);
+
+                    -- Put the instruction word on the bus
+                    IR <= temp_op;
+
+                    -- wait for the answer
+                    wait 20 ns;
+
+                --
+                -- INSTRUCTION: DEC
+                --
+
+
+                --
+                -- DONE LOOPING THROUGH INSTRUCTIONS! YAY
+                --
+                end if;
+
+                -----
+                --
+                -- NOW CHECK THE FLAGS
+                --
+                -----
+
+                --
+                -- CARRY FLAG
+                --
+                if ( op = ( OP_ADC  or OP_ADD   or OP_ADIW or
+                            OP_CP   or OP_CPC   or OP_CPI  or
+                            OP_NEG  or OP_SBC   or OP_SBCI or
+                            OP_SBIW or OP_SUB   or OP_SUBI)) then
+                    
+                    -- compute the carry flag
+                    check_bit := ((not rand_inptA(7)) and rand_inptB(7)) or (rand_inptB(7) and result(7)) or (result(7) and (not rand_inptA(7)));
+                    assert(StatRegs(0) = check_bit ) report "Adder Unit carry Flag incorrect";
+
+
+                elsif ( op = COM ) then
+                    assert(StatRegs(0) = '1') report "COM instruction carry flag incorrect";
+
+                elsif ( op = ( OP_ASR or OP_ROR or OP_LSR ) ) then
+                    assert(StatRegs(0) = rand_inptA(0)) report "Shifter Unit carry flag incorrect";
+
+                end if;
+
+                --
+                -- ZERO FLAG
+                --
+                if ( op /= ( OP_BCLR or OP_BLD   or OP_BSET or
+                                OP_BST  or OP_SWAP ) ) then
+
+                    if ( conv_integer(result) = 0 ) then
+                        check_bit := '1';
+                    else
+                        check_bit := '0';
+                    end if;
+
+                    assert(StatRegs(1) = check_bit) report "Zero flag incorrect";
+
+                end if;
+
+                --
+                -- NEGATIVE FLAG
+                --
+                if ( op /= ( OP_BCLR or OP_BLD  or OP_BSET or
+                             OP_BST  or OP_SWAP ) ) then
+
+                    assert(StatRegs(2) = result(7)) report "Negative Flag Incorrect";
+
+                end if;
+
+                --
+                -- SIGNED OVERFLOW FLAG
+                --
+
+                if ( op = ( OP_ADC  or OP_ADD  or OP_ADIW or
+                            OP_CPC  or OP_CPC  or OP_CPI  or 
+                            OP_DEC  or OP_INC  or OP_NEG  or
+                            OP_SBC  or OP_SBCI or OP_SBIW or
+                            OP_SUB  or OP_SUBI ) ) then
+
+                    check_bit := (rand_inptA(7) and not rand_inptB(7) and not result(7)) or (not rand_inptA(7) and rand_inptB(7) and result(7));
+                    assert(StatRegs(3) = check_bit) report "Arithmetic operation Signed Overflow Incorrect";
+
+                elsif( op =(OP_AND  or OP_ANDI or OP_COM  or
+                            OP_EOR  or OP_OR   or OP_ORI ) ) then
+                    assert(StatRegs(3) = '0') report "Logical operation Signed Overflow Incorrect";
+
+                elsif( op =(OP_ASR  or OP_LSR  or OP_ROR ) ) then
+                    check_bit := result(7) xor rand_inptA(0);
+                    assert(StatRegs(3) = check_bit) report "Shift operation signed overflow incorrect"
+
+                end if;
+
+                --
+                --  SIGN BIT
+                --
+
+                if ( op /= ( OP_BCLR or OP_BLD  or OP_BSET or
+                             OP_BST  or OP_SWAP ) ) then
+
+                    assert( StatRegs(4) = (StatRegs(3) xor StatRegs(2))) report "Sign flag incorrect";
+
+                end if;
+
+                --
+                -- HALF CARRY
+                --
+
+                if ( op = ( OP_ADD  or OP_ADC  or OP_CP   or
+                            OP_CPC  or OP_CPI  or OP_NEG  or 
+                            OP_SBC  or OP_SBCI or OP_SUB  or 
+                            OP_SUBI )) then
+
+                    check_bit := (not rand_inptA(3) and rand_inptB(3)) or (rand_inptB(3) and result(3)) or (not rand_inptA(3) and result(3));
+                    assert(StatRegs(5) = check_bit) report "half-carry flag incorrect"; 
+                
+                end if;
+
+            -- End the for loop
+            end loop;
+
+        -- End the while loop
+        end loop;
+
+    -- End the test process
+    end process do_test;
+
+-- All done describing the architecture
+end TB_ARCHITECTURE;
+
+
+
+
+
+
+
+
+
 
 
 
