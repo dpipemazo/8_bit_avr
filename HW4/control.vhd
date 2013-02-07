3f55c56bf348e45cf8c6895a83cc650594795613
dpipemazo <dpipemazo@gmail.com>
2013-02-06 23:54:30 -0800

    added definition for control unit

--- HW4/control.vhd
+++ HW4/control.vhd
@@ -13,11 +13,18 @@ use work.opcodes.all;
 entity Control is
 
     port(
-        IR      : in opcode_word;           -- Instruction register. Will 
-                                            -- delete this after HW4.
-        ProgDB  : in std_logic(15 downto 0) -- 
-
-
+        IR      : inout opcode_word;                -- Instruction register.Will 
+                                                    -- delete this after HW4.
+        ProgDB  : inout std_logic(15 downto 0);     -- Program Data Bus
+        SP      : out std_logic_vector(15 downto 0);-- stack pointer
+        MemCnst : out std_logic_vector(15 downto 0);-- memory constant from 
+                                                    -- LDS and STS instructions
+        WriteReg: out std_logic;                    -- write signal for regs.
+        RegInSel: out std_logic;                    -- Which input to use for 
+                                                    -- the registers. 0 = ALU, 
+                                                    -- 1 = Data Data Bus
+        CycleCnt: out std_logic(1 downto 0);        -- Which cycle of an 
+                                                    -- instruction we are on. 
     );
 
 
