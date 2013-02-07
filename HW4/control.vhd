2ed797ce9b85ed56da3f697a59aa27110a89e658
dpipemazo <dpipemazo@gmail.com>
2013-02-06 23:45:35 -0800

    added memteste.vhd

--- HW4/control.vhd
+++ HW4/control.vhd
@@ -1,3 +1,24 @@
 ---------------------------------------------------------------------
 -- Control Unit
 ---------------------------------------------------------------------
+
+-- Import IEEE libraries
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Import the custom libraries which Glen gave
+library work;
+use work.opcodes.all; 
+
+entity Control is
+
+    port(
+        IR      : in opcode_word;           -- Instruction register. Will 
+                                            -- delete this after HW4.
+        ProgDB  : in std_logic(15 downto 0) -- 
+
+
+    );
+
+
+
